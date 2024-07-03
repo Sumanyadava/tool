2bbc44e71bc52373e59c1651842cf3ac795afcf4
Sumanyadava <ansum2411@gmail.com>
2024-07-03 23:55:58 +0530

    (fix): miimum date in long todo

--- client/src/components/longTodo/TodoInput.jsx
+++ client/src/components/longTodo/TodoInput.jsx
@@ -15,12 +15,16 @@ const TodoInput = ({ elem }) => {
   const date = new Date();
   let day = date.getDate();
   let month = date.getMonth() + 1;
+  if (day< 10) {
+    day = "0" + day
+  }
   if (month < 10) {
     month = "0" + month;
   }
   let year = date.getFullYear();
 
   let currentDate = `${year}-${month}-${day}`;
+  console.log(currentDate);
 
   const dispatch = useDispatch();
 
